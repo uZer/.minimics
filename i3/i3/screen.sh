b4efb87c05dd673ef90a020e5684caa9b405a589
uZer <piolet.y@gmail.com>
2018-03-12 10:09:54 +0100

    feat(i3): change screen management

--- i3/i3/screen.sh
+++ i3/i3/screen.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
 # Also edit /usr/share/sddm/scripts/Xsetup
+#!/bin/sh
 intern=eDP1
 extern=HDMI2
+osones=DP2-2
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
+if xrandr | grep "$extern connected"; then
+    xrandr --output "$intern" --auto --output "$extern" --auto --right-of "$intern"
     nitrogen --restore 2>&1 > /dev/null
     # killall synergyc
+elif xrandr | grep "$osones connected"; then
+    xrandr --output "$intern" --auto --output "$osones" --auto --right-of "$intern"
+    nitrogen --restore 2>&1 > /dev/null
 else
-    xrandr --output "$intern" --auto --output "$extern" --auto --right-of "$intern"
+    xrandr --output "$extern" --off --output "$osones" --off --output "$intern" --auto
     nitrogen --restore 2>&1 > /dev/null
     # synergyc -n ovoid synergyserver.local
 fi
 
 # Prepare lockscreen
-./lock2.sh -u wallpaper
+# ./lock2.sh -u wallpaper
