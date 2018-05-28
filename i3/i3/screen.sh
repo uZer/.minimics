22ac79e88d9d4de5841cea23e3c758d31fcd2501
uZer <piolet.y@gmail.com>
2018-05-28 14:18:31 +0200

    feat(screen): handle thinkpad docks

--- i3/i3/screen.sh
+++ i3/i3/screen.sh
@@ -2,20 +2,43 @@
 #!/bin/sh
 intern=eDP1
 extern=HDMI2
-osones=DP2-2
+osones1=DP2-1
+osones2=DP2-2
 
 if xrandr | grep "$extern connected"; then
-    xrandr --output "$intern" --auto --output "$extern" --auto --right-of "$intern"
-    nitrogen --restore 2>&1 > /dev/null
-    # killall synergyc
-elif xrandr | grep "$osones connected"; then
-    xrandr --output "$intern" --auto --output "$osones" --auto --right-of "$intern"
-    nitrogen --restore 2>&1 > /dev/null
+    xrandr \
+      --output "$intern" --auto \
+      --output "$extern" --auto --right-of "$intern" \
+      --output "$osones1" --off \
+      --output "$osones2" --off
+elif xrandr | grep "$osones1 connected" && xrandr | grep "$osones2 disconnected"; then
+    xrandr \
+      --output "$intern" --auto \
+      --output "$extern" --off \
+      --output "$osones1" --auto --left-of "$intern" \
+      --output "$osones2" --off
+
+elif xrandr | grep "$osones2 connected" && xrandr | grep "$osones1 disconnected"; then
+    xrandr \
+      --output "$intern" --auto \
+      --output "$extern" --off \
+      --output "$osones1" --off \
+      --output "$osones2" --auto --left-of "$intern"
+elif xrandr | grep "$osones1 connected" && xrandr | grep "$osones2 connected"; then
+    xrandr \
+      --output "$intern" --auto \
+      --output "$extern" --off \
+      --output "$osones1" --auto --left-of "$intern" \
+      --output "$osones2" --auto --left-of "$osones1"
 else
-    xrandr --output "$extern" --off --output "$osones" --off --output "$intern" --auto
-    nitrogen --restore 2>&1 > /dev/null
-    # synergyc -n ovoid synergyserver.local
+    xrandr \
+      --output "$intern" --auto \
+      --output "$extern" --off \
+      --output "$osones1" --off \
+      --output "$osones2" --off
 fi
+
+nitrogen --restore 2>&1 > /dev/null
 
 # Prepare lockscreen
 # ./lock2.sh -u wallpaper
