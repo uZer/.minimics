8f7f1b3501609e0ab2c482ce98f94cffd795d55a
uZer <piolet.y@gmail.com>
2021-05-09 17:13:08 +0200

    feat(polybar): add polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/polybar"
+style="advancedblocks"
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	polybar -q main -c "$dir/$style/config.ini" &
+}
+
+launch_bar
