a5b395c9905186a47d23e4c5842cd4ce61cba0af
uZer <piolet.y@gmail.com>
2021-05-16 03:55:13 +0200

    maint(vim): cleanup vimrc file

--- polybar/advancedblocks/scripts/weather.sh
+++ polybar/advancedblocks/scripts/weather.sh
@@ -10,7 +10,7 @@ fi
 if [ "$param" = "moon" ]; then
   out=$(curl -sS "https://wttr.in/${MINIMICS_CITY:Paris}?format=%m" 2> /dev/null)
 else
-  out=$(curl -sf "https://wttr.in/${MINIMICS_CITY:Paris}?0pq&lang=fr&format=1" 2> /dev/null | sed 's/+/ /g' | sed 's/:/ /g')
+  out=$(curl -sf "https://wttr.in/${MINIMICS_CITY:Paris}?0pq&lang=fr&format=1" 2> /dev/null | sed 's/+//g' | sed 's/://g')
 fi
 
 # Display
