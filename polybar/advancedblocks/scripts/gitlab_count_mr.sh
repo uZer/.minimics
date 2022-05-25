d97b8fb034b94bdf07a4430a802e023643be9631
uZer <piolet.y@gmail.com>
2022-05-25 22:55:52 +0200

    fix(polybar): update gitlab script to pass private token as a header

--- polybar/advancedblocks/scripts/gitlab_count_mr.sh
+++ polybar/advancedblocks/scripts/gitlab_count_mr.sh
@@ -8,10 +8,10 @@ source ~/.minimicsrc
 count_mr ()
 {
 	# Count Merge Requests with label "Ready For Review"
-  review_count=$(curl -sf "${MINIMICS_GITLAB_API_URL}/${1}/merge_requests?scope=all&state=opened&wip=no&labels=ready%20for%20review&view=simple&private_token=${MINIMICS_GITLAB_PRIVATE_TOKEN}" | jq length)
+  review_count=$(curl -sf -H "x-per-page: 99" -H "PRIVATE-TOKEN: ${MINIMICS_GITLAB_PRIVATE_TOKEN}" "${MINIMICS_GITLAB_API_URL}/${1}/merge_requests/?scope=all&state=opened&draft=no&labels=status%3A%3Aready%20for%20review&view=simple" | jq length)
 
 	# Count Merge Requests with label "Ready For Merge"
-  merge_count=$(curl -sf "${MINIMICS_GITLAB_API_URL}/${1}/merge_requests?scope=all&state=opened&wip=no&labels=ready%20for%20merge&view=simple&private_token=${MINIMICS_GITLAB_PRIVATE_TOKEN}" | jq length)
+  merge_count=$(curl -sf  -H "x-per-page: 99" -H "PRIVATE-TOKEN: ${MINIMICS_GITLAB_PRIVATE_TOKEN}" "${MINIMICS_GITLAB_API_URL}/${1}/merge_requests?scope=all&state=opened&draft=no&labels=status%3A%3Aready%20for%20merge&view=simple" | jq length)
 
   echo " ${review_count}  ${merge_count}"
 }
