e918131529d96b35be91390a1b24220a19c47ede
uZer <piolet.y@gmail.com>
2024-01-23 23:37:38 +0100

    feat(hypr): add tessen for password autotyping

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@
 #   hyprland:
 #     xdg-desktop-portal-hyprland wofi swaylock-effects swaybg slurp grim
 #     wl-clipboard wireplumber pipewire pipewire-pulse wdisplays kanshi
-#     playerctl brightnessctl hyprpicker playerctl xdg-utils copyq
+#     playerctl brightnessctl hyprpicker playerctl xdg-utils copyq tessen
+#     wtype
 #   i3:
 #     i3blocks i3status i3lock-color picom polybar rofi rofi-pass playerctl
 #     wireplumber pipewire pipewire-pulse brightnessctl playerctl
