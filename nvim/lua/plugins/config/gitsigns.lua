a09fe82349ea514b2bdf803a6dc1ad5e01c84300
uZer <piolet.y@gmail.com>
2024-06-23 00:35:13 +0200

    fix(nvim): remove obsolete gitsigns date format setting

--- nvim/lua/plugins/config/gitsigns.lua
+++ nvim/lua/plugins/config/gitsigns.lua
@@ -1,9 +1,6 @@
 local options = {
   current_line_blame = true,
   current_line_blame_formatter = " <author>, <author_time:%Y/%m/%d %H:%M>  <abbrev_sha>  <summary>",
-  current_line_blame_formatter_opts = {
-    relative_time = false,
-  },
   signcolumn = true,
   numhl = true,
   linehl = false,
