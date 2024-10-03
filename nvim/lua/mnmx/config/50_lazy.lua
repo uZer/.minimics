18b64b93a56e26795184019c8499750a40230e86
Youenn Piolet <piolet.y@gmail.com>
2024-10-03 19:28:25 +0200

    fix(nvim): remove error message at the start of nvim

--- nvim/lua/mnmx/config/50_lazy.lua
+++ nvim/lua/mnmx/config/50_lazy.lua
@@ -2,16 +2,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = { { import = "mnmx.plugins" } },
-  install = { colorscheme = { "pywal16" } },
+  spec = {
+    {
+      import = "mnmx.plugins"
+    }
+  },
+  install = {
+    colorscheme = { "pywal16" }
+  },
   defaults = {
     lazy = false,
-    version = false,
+    -- version = false, -- commented to prevent error messages at nvim startup
   },
   checker = {
     enabled = true,
