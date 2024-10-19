9cd3ad21f93c6bb8ad1767ac6402cb0d01aa0c13
Youenn Piolet <piolet.y@gmail.com>
2024-10-20 01:35:59 +0200

    feat(mini): add scaffolding projects and mini init

---
+++ bin/lib.oops.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Fail with an error message
+# Useful with piped OR operator:
+#   command || oops "exit with a message"
+#   command || oops "exit with return code 12" 12
+oops() {
+  logg err "$1"
+  if [ $# -eq 2 ]; then
+    exit "$2"
+  else
+    exit 10
+  fi
+}
+
+export -f oops
---
+++ scaffolding/go/README.md
@@ -0,0 +1,19 @@
+# {{mod_name}}
+
+WIP
+
+<!--toc:start-->
+- [{{mod_name}}](#{{mod_name}})
+  - [Requirements](#requirements)
+  - [Development](#development)
+<!--toc:end-->
+
+## Requirements
+
+## Development
+
+Use Makefile if needed
+
+```
+make
+```
