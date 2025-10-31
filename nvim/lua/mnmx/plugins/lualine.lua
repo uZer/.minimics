58ba851b44a820dcac9eaf822b6d7eac71c23eaf
Youenn Piolet <piolet.y@gmail.com>
2025-10-31 13:05:28 +0100

    feat(nvim): improve lint/lsp display in lualine

--- nvim/lua/mnmx/plugins/lualine.lua
+++ nvim/lua/mnmx/plugins/lualine.lua
@@ -1,85 +1,139 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'arkav/lualine-lsp-progress',
-    'ojroques/nvim-bufdel'
+  {
+    'linrongbin16/lsp-progress.nvim',
+    config = function()
+      require('lsp-progress').setup()
+      -- listen lsp-progress event and refresh lualine
+      vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+      vim.api.nvim_create_autocmd("User", {
+        group = "lualine_augroup",
+        pattern = "LspProgressStatusUpdated",
+        callback = require("lualine").refresh,
+      })
+    end
   },
-  opts = {
-    options = {
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
-      disable_filetypes = {
-        "NvimTree",
-        "lazy",
-        "mason",
-        "_.*",
-      },
-      extensions = {
-        'lazy',
-        'nvim-tree',
-        'mason',
-      },
-      ignore_focus = {
-        "NvimTree",
-        "lazy",
-        "mason",
-        "_.*",
-      },
-      theme = 'pywal16-nvim',
-      globalstatus = true,
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'ojroques/nvim-bufdel'
     },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {
-        {
-          'filename',
-          path = 3, -- use absolute paths with tilde as home
-          newfile_status = true,
-        }
-      },
-      lualine_x = {},
-      lualine_y = { 'filetype', 'encoding', 'fileformat' },
-      lualine_z = { 'progress', 'location' },
-    },
-
-    tabline = {
-      lualine_a = {
-        {
-          'buffers',
-          filetype_names = {
-            NvimTree = '',
-            mason = '',
-            lazy = ''
-          },
-          mode = 2,
-          symbols = {
-            alternate_file = '',
-          }
-        }
-      },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {
-        {
-          'lsp_progress',
-        }
-      },
-      lualine_y = {
-        {
-          'diagnostics',
-          sources        = { 'nvim_diagnostic' },
-          always_visible = false,
+    opts = {
+      options = {
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disable_filetypes = {
+          "NvimTree",
+          "lazy",
+          "mason",
+          "_.*",
         },
-        {
-          'lsp_status'
-        }
+        extensions = {
+          'lazy',
+          'nvim-tree',
+          'mason',
+        },
+        ignore_focus = {
+          "NvimTree",
+          "lazy",
+          "mason",
+          "_.*",
+        },
+        theme = 'pywal16-nvim',
+        globalstatus = true,
       },
-      lualine_z = {
-        {
-          'searchcount',
-        }
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+          {
+            'filename',
+            path = 3, -- use absolute paths with tilde as home
+            newfile_status = true,
+          }
+        },
+        lualine_x = {},
+        lualine_y = {
+          'encoding',
+          'fileformat'
+        },
+        lualine_z = {
+          'progress',
+          'location',
+          'searchcount'
+        },
       },
-    },
-  }
+
+      tabline = {
+        lualine_a = {
+          {
+            'buffers',
+            filetype_names = {
+              NvimTree = '',
+              mason = '',
+              lazy = ''
+            },
+            mode = 2,
+            symbols = {
+              alternate_file = '',
+            }
+          }
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {
+          {
+            'diagnostics',
+            sources        = { 'nvim_diagnostic' },
+            always_visible = false,
+          },
+          {
+            function()
+              return require("lsp-progress").progress({
+                max_size = 80,
+                format = function(messages)
+                  local active_clients =
+                      vim.lsp.get_clients()
+                  if #messages > 0 then
+                    return table.concat(messages, " ")
+                  end
+                  local client_names = {}
+                  for _, client in ipairs(active_clients) do
+                    if client and client.name ~= "" then
+                      table.insert(
+                        client_names,
+                        1,
+                        client.name
+                      )
+                    end
+                  end
+                  return table.concat(client_names, "  ")
+                end,
+              })
+            end,
+            icon = { "", align = "left" },
+          },
+
+        },
+        lualine_y = {
+          {
+            function()
+              local linters = require('lint')._resolve_linter_by_ft(vim.bo.filetype)
+              if not linters or #linters == 0 then
+                -- no linter
+                return ''
+              end
+              return table.concat(linters, '  ')
+            end,
+            icon = { "󰱺", align = "left" },
+          },
+        },
+        lualine_z = {
+          {
+            'filetype',
+            icon = { align = "left" }
+          }
+        },
+      },
+    }
+  },
 }
