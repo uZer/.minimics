61f14d6982410e80fa75d555944b2e718dc1afba
Youenn Piolet <piolet.y@gmail.com>
2025-11-03 21:43:43 +0100

    feat(nvim): try notification system

---
+++ nvim/lua/mnmx/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  keys = {
+    {
+      "?",
+      "<cmd>Notifications<cr>",
+      desc = "show history of notifications"
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    -- "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    -- "nvim-mini/mini.notify"
+  }
+}
