628b125b9e755659840f67fd96938757e0667887
Youenn Piolet <piolet.y@gmail.com>
2026-01-19 17:50:27 +0100

    feat(nvim): leak custom gitlab URL and use osc52 for clipboard

--- nvim/lua/mnmx/plugins/gitlinker.lua
+++ nvim/lua/mnmx/plugins/gitlinker.lua
@@ -1,9 +1,24 @@
 return {
   "linrongbin16/gitlinker.nvim",
   cmd = "GitLink",
-  opts = {},
+  opts = function()
+    return {
+      router = {
+        browse = {
+          ["^gitlab%.dnm%.radiofrance%.fr"] = require("gitlinker.routers").gitlab_browse,
+        },
+        blame = {
+          ["^gitlab%.dnm%.radiofrance%.fr"] = require("gitlinker.routers").gitlab_blame,
+        },
+      },
+      -- Neovim ≥ 0.10 native osc52
+      clipboard_override = function(url)
+        require("vim.ui.clipboard.osc52").copy("+")({ url })
+      end,
+    }
+  end,
   keys = {
-    { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
-    { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    { "gy", "<cmd>GitLink blame<cr>",  mode = { "n", "v" }, desc = "Yank git blame link" },
+    { "gY", "<cmd>GitLink! blame<cr>", mode = { "n", "v" }, desc = "Open git blame link" },
   },
 }
