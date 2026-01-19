d055b9b42185385530700475e8ada56dd3f1fd60
Youenn Piolet <piolet.y@gmail.com>
2026-01-19 16:16:51 +0100

    feat(nvim): add gitlinker

---
+++ nvim/lua/mnmx/plugins/gitlinker.lua
@@ -0,0 +1,9 @@
+return {
+  "linrongbin16/gitlinker.nvim",
+  cmd = "GitLink",
+  opts = {},
+  keys = {
+    { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
+    { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+  },
+}
