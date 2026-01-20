412095ba413a1e872bd631e3d44d519dbeb6d7f6
Youenn Piolet <piolet.y@gmail.com>
2026-01-20 18:02:33 +0100

    feat(nvim): add telescope fzf dependency

--- nvim/lua/mnmx/plugins/telescope.lua
+++ nvim/lua/mnmx/plugins/telescope.lua
@@ -2,6 +2,10 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
   },
   opts = {
     extensions = {
