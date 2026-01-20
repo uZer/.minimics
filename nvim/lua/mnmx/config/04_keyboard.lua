647f3c620bfdfa316c5cab5e466f494e5ffdf2f3
Youenn Piolet <piolet.y@gmail.com>
2026-01-20 18:54:12 +0100

    feat(nvim): cleanup keymaps

--- nvim/lua/mnmx/config/04_keyboard.lua
+++ nvim/lua/mnmx/config/04_keyboard.lua
@@ -2,76 +2,104 @@
 -- Keyboard options 
 --
 
--- create a local map function to ease key mapping declarations
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+-- convenience wrapper
+local map = function(mode, lhs, rhs, opts)
+  opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+  vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 -- leader is <space> bar
 vim.g.mapleader = ' '
 vim.g.maplocalleader = "\\"
 
--- switch CWD to the directory of the open buffer
-map('', '<C-w>cd', ':cd %:p:h<cr>:pwd<cr>')
+-- switch CWD to the directory of the current buffer
+map('n', '<C-w>cd', function()
+  vim.cmd.cd('%:p:h')
+  vim.cmd.pwd()
+end)
 
 -- yank to clipboard (OSC 52)
 -- no paste support due to security concerns (see alacritty configuration)
 -- so please use terminal paste with shift+insert
-map('', 'Y', '"+y')
+map({ 'n', 'v' }, 'Y', '"+y')
 
 -- window management
-map('', '<leader>wj', '<C-w>j') -- move to window...
-map('', '<leader>wk', '<C-w>k')
-map('', '<leader>wh', '<C-w>h')
-map('', '<leader>wl', '<C-w>l')
-map('', '<leader>wn', ':vsplit<cr>')
-map('', '<leader>wb', ':split<cr>')
-map('', '<leader>wc', ':close<cr>')
-map('', '<leader>wJ', '<C-w>J') -- move window...
-map('', '<leader>wK', '<C-w>K')
-map('', '<leader>wH', '<C-w>H')
-map('', '<leader>wL', '<C-w>L')
+map('n', '<leader>wj', '<C-w>j')
+map('n', '<leader>wk', '<C-w>k')
+map('n', '<leader>wh', '<C-w>h')
+map('n', '<leader>wl', '<C-w>l')
+map('n', '<leader>wn', '<cmd>vsplit<cr>')
+map('n', '<leader>wb', '<cmd>split<cr>')
+map('n', '<leader>wc', '<cmd>close<cr>')
+map('n', '<leader>wJ', '<C-w>J')
+map('n', '<leader>wK', '<C-w>K')
+map('n', '<leader>wH', '<C-w>H')
+map('n', '<leader>wL', '<C-w>L')
 
 -- press tab to autoindent
-map('n', '<TAB>', '==')
-map('v', '<TAB>', '=')
+map('n', '<Tab>', '==')
+map('v', '<Tab>', '=')
 
 -- movements
-map('', '<C-h>', 'b')
-map('', '<C-l>', 'w')
-map('', '<C-a>', '^')
-map('', '<C-e>', '$')
+map({ 'n', 'v' }, '<C-h>', 'b')
+map({ 'n', 'v' }, '<C-l>', 'w')
+map({ 'n', 'v' }, '<C-a>', '^')
+map({ 'n', 'v' }, '<C-e>', '$')
 
 -- treat long lines as break lines (useful when moving around in them)
-map('', 'j', 'gj')
-map('', 'k', 'gk')
+map({ 'n', 'v' }, 'j', 'gj')
+map({ 'n', 'v' }, 'k', 'gk')
 
 -- move entire lines up and down with ctrl+{j, k}
-map('n', '<C-j>', ':m .+1<CR>==')
-map('n', '<C-k>', ':m .-2<CR>==')
-map('v', '<C-j>', ":m '>+1<CR>gv=gv")
-map('v', '<C-k>', ":m '<-2<CR>gv=gv")
-map('i', '<C-j>', '<Esc>:m .+1<CR>==gi')
-map('i', '<C-k>', '<Esc>:m .-2<CR>==gi')
+map('n', '<C-j>', '<cmd>m .+1<cr>==')
+map('n', '<C-k>', '<cmd>m .-2<cr>==')
+map('v', '<C-j>', "<cmd>m '>+1<cr>gv=gv")
+map('v', '<C-k>', "<cmd>m '<-2<cr>gv=gv")
+map('i', '<C-j>', '<Esc><cmd>m .+1<cr>==gi')
+map('i', '<C-k>', '<Esc><cmd>m .-2<cr>==gi')
 
 -- spell check
-map('', '<leader>sc', ':set invspell<CR>')
+map('n', '<leader>sc', '<cmd>set invspell<cr>')
 
 -- buffers, files & tabs management
-map('', '<F2>', ':bprev<cr>')
-map('', '<F3>', ':bnext<cr>')
-map('', '<C-S-Tab>', ':bprev<cr>')
-map('', '<C-Tab>', ':bnext<cr>')
-map('', '<leader>tn', ':enew<cr>')
-map('', '<leader>tc', ':BufDel<cr>') -- [plugin: bufdel]
+map('n', '<F2>', '<cmd>bprev<cr>')
+map('n', '<F3>', '<cmd>bnext<cr>')
+map('n', '<C-Tab>', '<cmd>bnext<cr>')
+map('n', '<C-S-Tab>', '<cmd>bprev<cr>')
+map('n', '<leader>tn', '<cmd>enew<cr>')
+map('n', '<leader>tc', '<cmd>BufDel<cr>') -- [plugin: bufdel]
 
 -- [plugin: telescope] find stuff
-map('', '<C-o>', ':Telescope find_files<CR>')
-map('', '<C-f>', ':lua require("telescope.builtin").live_grep({grep_open_files=false})<CR>')
-map('', '<C-g>', ':lua require("telescope.builtin").grep_string<CR>')
-map('', '<C-p>', ':Telescope<CR>')
-map('', '<leader>p', ':Telescope<CR>')
+map('n', '<C-o>', '<cmd>Telescope find_files<cr>')
+map('n', '<C-p>', '<cmd>Telescope<cr>')
+map('n', '<leader>p', '<cmd>Telescope<cr>')
+map('n', '<C-g>', function()
+  require('telescope.builtin').live_grep({ grep_open_files = false })
+end)
+
+map('n', '<C-f>', function()
+  require('telescope.builtin').grep_string({ grep_open_files = false })
+end)
+
+local function getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg("v")
+  vim.fn.setreg("v", {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ""
+  end
+end
+
+map('v', '<C-f>', function()
+  require("telescope.builtin").live_grep({
+    default_text = getVisualSelection(),
+    only_sort_text = true,
+    additional_args = function()
+      return { "--pcre2" }
+    end,
+  })
+end)
