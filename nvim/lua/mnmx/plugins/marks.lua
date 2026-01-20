c85f6c0f1837d02fa27a015abb27067adb954cd8
Youenn Piolet <piolet.y@gmail.com>
2026-01-20 18:01:04 +0100

    feat(nvim): new marks shotcuts

--- nvim/lua/mnmx/plugins/marks.lua
+++ nvim/lua/mnmx/plugins/marks.lua
@@ -3,7 +3,15 @@ return {
   event = "VeryLazy",
   opts = {
     mappings = {
-      annotate = "<C-w>m",
+
+      preview = "mm",
+      prev = "mk",
+      next = "mj",
+
+      -- bookmarks won't persist after nvim restart
+      prev_bookmark = "mK",
+      next_bookmark = "mJ",
+      annotate = "<C-w>m", -- only works with bookmarks (marks 0-9)
     },
   },
 }
