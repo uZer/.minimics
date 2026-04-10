946d2d875a92ee365b4d82cdde6ffb93b6fd6c07
Youenn Piolet <piolet.y@gmail.com>
2026-04-10 17:32:04 +0200

    feat(nvim): improve indent on save and modernize some settings

--- nvim/lua/mnmx/config/01_behavior.lua
+++ nvim/lua/mnmx/config/01_behavior.lua
@@ -3,28 +3,32 @@
 --
 
 -- loglevel
-vim.lsp.set_log_level("ERROR")
+vim.lsp.log.set_level("ERROR")
 
 -- Show special chars
 vim.opt.list = true
-vim.opt.listchars = "tab:| ,trail:-,nbsp:+"
+vim.opt.listchars = {
+  tab = "│ ",
+  trail = "·",
+  nbsp = "+",
+}
 
 -- Indentation and tabulations
-vim.opt.tabstop = 2        -- a tab is two spaces
-vim.opt.shiftwidth = 2     -- indentation is two spaces
-vim.opt.expandtab = true   -- no tab char in the files, use spaces
-vim.opt.autoindent = true  -- keep current indentation after pressing <cr>
+vim.opt.tabstop = 2 -- a tab is two spaces
+vim.opt.shiftwidth = 2 -- indentation is two spaces
+vim.opt.expandtab = true -- no tab char in the files, use spaces
+vim.opt.autoindent = true -- keep current indentation after pressing <cr>
 vim.opt.smartindent = true -- indent accordingly to the syntax
-vim.opt.smarttab = true    -- indent by shiftwidth spaces when pressing tab
+vim.opt.smarttab = true -- indent by shiftwidth spaces when pressing tab
 
 -- Folding and Conceal
 vim.opt.foldenable = false -- disable folding by default
 
 -- Search
-vim.opt.hlsearch = true   -- highlight search patterns
-vim.opt.incsearch = true  -- go to matches when searching
+vim.opt.hlsearch = true -- highlight search patterns
+vim.opt.incsearch = true -- go to matches when searching
 vim.opt.ignorecase = true -- ignore case in search pattern
-vim.opt.smartcase = true  -- check case if the pattern contains uppercase chars
+vim.opt.smartcase = true -- check case if the pattern contains uppercase chars
 
 -- Copy / Paste settings
 -- My terminal is alacritty now. It doesn't support runtime capability detection
@@ -35,50 +39,50 @@ vim.opt.smartcase = true  -- check case if the pattern contains uppercase chars
 --
 -- To paste from terminal, use shift+insert (pasteSelection).
 -- To copy to selection/primary, use "+y or Y (Yy for full line)
-local osc52 = require('vim.ui.clipboard.osc52')
-vim.g.clipboard = {
-  name = 'osc52-write',
-  copy = {
-    ['+'] = osc52.copy('+'),
-    ['*'] = osc52.copy('*'),
-  },
-  paste = {
-    ['+'] = function() return { {}, 'v' } end,
-    ['*'] = function() return { {}, 'v' } end,
-  },
-}
+if vim.fn.has("clipboard") == 0 then
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "osc52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = function()
+        return { {}, "v" }
+      end,
+      ["*"] = function()
+        return { {}, "v" }
+      end,
+    },
+  }
+end
 
 -- Spell
-vim.opt.spelllang = 'en_us,fr'
+vim.opt.spelllang = "en_us,fr"
 vim.opt.spell = false -- enable spell check with <leader>ss
 
 -- Other
-vim.opt.fileformats = { "unix", "dos", "mac" }         -- use Unix first
-vim.opt.textwidth = 80                                 -- for `gq` and linebreak text at 80 columns when typing
-vim.opt.scrolloff = 7                                  -- scroll when at n lines from the borders of the buffer
-vim.opt.swapfile = false                               -- reduce pollution, save the planet
-vim.opt.wildignore = { "*.o", "*.obj", "*~", "*.pyc" } -- ignore some files
-
-local augroup = vim.api.nvim_create_augroup            -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd            -- Create autocommand
+vim.opt.fileformats = { "unix", "dos" } -- use Unix first
+vim.opt.textwidth = 80 -- for `gq` and linebreak text at 80 columns when typing
+vim.opt.scrolloff = 8 -- scroll when at n lines from the borders of the buffer
+vim.opt.sidescrolloff = 8
+vim.opt.swapfile = false -- reduce pollution, save the planet
+vim.opt.wildignore:append({ "*.o", "*.obj", "*~", "*.pyc" }) -- ignore some files
+vim.opt.wildignorecase = true
+vim.opt.inccommand = "split" -- live preview of substitutions
 
 -- Filetype specific: fix indentation and remove tabs
-augroup('setIndent', { clear = true })
-autocmd('Filetype', {
-  group = 'setIndent',
+local set_indent_group = vim.api.nvim_create_augroup("setIndent", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = set_indent_group,
   pattern = {
-    'css',
-    'html',
-    'javascript',
-    'lisp',
-    'lua',
-    'scss',
-    'typescript',
-    'xhtml',
-    'xml',
-    'yaml',
+    "go",
+    "make",
   },
-  command = 'setlocal shiftwidth=2 tabstop=2 expandtab'
+  callback = function()
+    vim.opt_local.expandtab = false
+  end,
 })
 
 vim.filetype.add({
@@ -110,16 +114,12 @@ vim.filetype.add({
 })
 
 -- Return to last edit position when opening files
-vim.cmd([[
-autocmd BufReadPost *
-  \ if line("'\"") > 0 && line("'\"") <= line("$") |
-  \   exe "normal! g`\"" |
-  \ endif
-]])
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      vim.cmd('normal! g`"')
+    end
   end,
 })
--- nvim/lua/mnmx/plugins/conform.lua
+++ nvim/lua/mnmx/plugins/conform.lua
@@ -12,37 +12,36 @@ return {
   end,
   opts = {
     default_format_opts = {
-      lsp_format = "first"
+      lsp_format = "fallback",
     },
     formatters_by_ft = {
       ["*"] = { "trim_whitespace" },
-      ua = { "stylua" },
+      lua = { "stylua" },
       go = { "goimports", "gofmt" },
       jsonnet = { "jsonnetfmt" },
       liquidsoap = { "liquidsoap-prettier" },
     },
-    -- format_on_save = {
-    --   timeout_ms = 500,
-    --   lsp_format = "fallback",
-    -- },
+
     format_on_save = function(bufnr)
-      if slow_format_filetypes[vim.bo[bufnr].filetype] then
+      local ft = vim.bo[bufnr].filetype
+
+      if slow_format_filetypes[ft] then
         return
       end
-      local function on_format(err)
-        if err and err:match("timeout$") then
-          slow_format_filetypes[vim.bo[bufnr].filetype] = true
-        end
-      end
 
-      return { timeout_ms = 5000, lsp_format = "fallback" }, on_format
+      return {
+        timeout_ms = 5000,
+        lsp_format = "fallback",
+      }
     end,
 
     format_after_save = function(bufnr)
-      if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-        return
+      if slow_format_filetypes[vim.bo[bufnr].filetype] then
+        return {
+          timeout_ms = 15000,
+          lsp_format = "fallback",
+        }
       end
-      return { timeout_ms = 15000, lsp_format = "fallback" }
     end,
-  }
+  },
 }
