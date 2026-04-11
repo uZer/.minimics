f3f79414939ffc3894b552278eff28484f48931e
Youenn Piolet <piolet.y@gmail.com>
2026-04-12 01:41:06 +0200

    feat(nvim): improve greatly nvim-cmp configuration

--- nvim/lua/mnmx/plugins/nvim-cmp.lua
+++ nvim/lua/mnmx/plugins/nvim-cmp.lua
@@ -1,105 +1,150 @@
 return {
-  --   'VonHeikemen/lsp-zero.nvim',
-  --   branch = 'v3.x',
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+
   dependencies = {
-    -- LSP Support
-    'neovim/nvim-lspconfig',
-    'mason-org/mason-lspconfig.nvim',
-    'b0o/SchemaStore.nvim',
+    -- LSP
+    "neovim/nvim-lspconfig",
+    "mason-org/mason-lspconfig.nvim",
+    "b0o/SchemaStore.nvim",
 
-    -- Autocompletion
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lua',
-    'davidsierradz/cmp-conventionalcommits',
-    'onsails/lspkind.nvim',
+    -- Completion sources
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua",
+    {
+      "davidsierradz/cmp-conventionalcommits",
+      ft = "gitcommit",
+    },
 
-    -- -- Snippets
-    'saadparwaiz1/cmp_luasnip',
-    'L3MON4D3/LuaSnip',
-    'rafamadriz/friendly-snippets',
+    -- UI
+    "onsails/lspkind.nvim",
+
+    -- Snippets
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
   },
 
   opts = function()
-    local cmp = require('cmp')
-    local lspkind = require('lspkind')
-    local luasnip = require('luasnip')
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
-    -- repair tabs in insert mode
-    ---@diagnostic disable-next-line: deprecated
-    table.unpack = table.unpack or unpack -- Lua 5.1 compatibility
+    require("luasnip.loaders.from_vscode").lazy_load()
 
     local has_words_before = function()
-      local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
     return {
-      confirmation = {
-        -- completeopt = 'menu,menuone,noinsert,noselect',
-        completeopt = 'menu,menuone,noinsert,noselect',
-        default_behavior = cmp.ConfirmBehavior.Replace,
+      performance = {
+        debounce = 60,
+        throttle = 30,
+        fetching_timeout = 200,
+        max_view_entries = 40,
+      },
+
+      completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+        keyword_length = 2,
+      },
+
+      experimental = {
+        ghost_text = true, -- inline suggestions
+      },
+
+      preselect = cmp.PreselectMode.None, -- strict behavior
+
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
 
       formatting = {
         format = lspkind.cmp_format({
-          mode = 'symbol_text',
+          mode = "symbol_text",
           maxwidth = 50,
-          ellipsis_char = '...',
-          before = function(_, vim_item)
-            return vim_item
-          end
-        })
+          ellipsis_char = "...",
+        }),
       },
 
-      sources = {
-        { name = 'buffer',                 keyword_length = 3 },
-        { name = 'path',                   option = { trailing_slash = true }, },
-        { name = 'conventionalcommits' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'nvim_lua' },
-        { name = 'luasnip' },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp_signature_help", priority = 1100 },
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 800 },
+        { name = "nvim_lua", priority = 600 },
+        { name = "path", priority = 400 },
+        { name = "conventionalcommits", priority = 300 },
+      }, {
+        {
+          name = "buffer",
+          priority = 100,
+          keyword_length = 3,
+          option = {
+            -- Only use visible buffers (huge perf win)
+            get_bufnrs = function()
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end,
+          },
+        },
+      }),
+
+      -- Better sorting
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.kind,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
       },
 
       window = {
         completion = {
           scrollbar = false,
-        }
+          border = "rounded",
+        },
+        documentation = {
+          border = "rounded",
+        },
       },
 
-      mapping = {
-        ['<C-D>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.scroll_docs(-2)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+      mapping = cmp.mapping.preset.insert({
 
-        ['<C-F>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.scroll_docs(2)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+        -- Docs
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-        ['<C-Space>'] = cmp.mapping(function(_)
-          if cmp.visible() then
-            cmp.confirm({
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            })
-          else
-            cmp.complete()
-          end
-        end, { "i", "s" }),
+        -- Trigger
+        ["<C-Space>"] = cmp.mapping.complete(),
 
+        -- Abort
+        ["<C-e>"] = cmp.mapping.abort(),
+
+        -- Confirm (explicit, safe)
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }),
+
+        -- Navigation
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+
+        -- Tab logic (correct hierarchy)
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
@@ -112,35 +157,54 @@ return {
           end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function()
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
-          elseif has_words_before() then
-            cmp.complete()
-          end
-        end, { "i", "s" }),
-
-        ["<C-J>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
           else
             fallback()
           end
         end, { "i", "s" }),
-
-        ["<C-K>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        -- Remove mapping of <CR>
-        ['<CR>'] = function(fallback) fallback() end
-      },
+      }),
     }
+  end,
+
+  config = function(_, opts)
+    local cmp = require("cmp")
+    cmp.setup(opts)
+
+    -- Cmdline completion
+
+    -- Search (/)
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- Command (:)
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+
+    -- Git commit completion
+    cmp.setup.filetype("gitcommit", {
+      completion = {
+        keyword_length = 1, -- trigger early
+      },
+      sources = cmp.config.sources({
+        { name = "conventionalcommits", priority = 1000 },
+        { name = "luasnip", priority = 800 },
+      }, {
+        { name = "buffer", keyword_length = 3, priority = 100 },
+      }),
+    })
   end,
 }
