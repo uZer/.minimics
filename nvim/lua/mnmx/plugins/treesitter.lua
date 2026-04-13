7190969679d2f034267be951a4d97e5d4f394362
Youenn Piolet <piolet.y@gmail.com>
2026-04-13 14:12:21 +0200

    feat(nvim): update treesitter (broken)

--- nvim/lua/mnmx/plugins/treesitter.lua
+++ nvim/lua/mnmx/plugins/treesitter.lua
@@ -1,42 +1,96 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  branch = "master",
-  build = ":TSUpdate",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      modules = {}, -- required by TSConfig type checker
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "MeanderingProgrammer/treesitter-modules.nvim",
+    },
+    config = function()
+      local languages = {
         "bash",
+        "c",
+        "css",
+        "diff",
         "dockerfile",
+        "gitignore",
         "go",
         "gomod",
         "gosum",
+        "graphql",
+        "html",
         "hyprlang",
+        "javascript",
+        "jsdoc",
         "json",
         "jsonnet",
         "liquidsoap",
         "lua",
+        "lua",
+        "luadoc",
+        "luap",
         "markdown",
         "markdown_inline",
+        "printf",
         "python",
+        "query",
         "regex",
+        "regex",
+        "sql",
         "terraform",
         "toml",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
         "vimdoc",
+        "xml",
         "yaml",
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+      }
+
+      -- Covers ensure_installed + highlight + indent + fold + incremental selection
+      require("treesitter-modules").setup({
+        ensure_installed = languages,
+        ignore_install = {},
+        sync_install = false,
+        auto_install = false,
+
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        fold = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
+
+      -- Fold settings
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+      -- textobjects plugin now uses its own setup + keymaps
+      require("nvim-treesitter-textobjects").setup({
+        move = {
+          set_jumps = false,
+        },
+        select = {
+          lookahead = true,
+        },
+      })
+    end,
+  },
 }
