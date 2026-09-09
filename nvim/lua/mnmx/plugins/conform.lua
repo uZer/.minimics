008ec894f476950a88d0be83c2c44aaa78a960a5
Youenn Piolet <piolet.y@gmail.com>
2026-09-09 23:02:06 +0200

    feat(nvim): improve conform configuration

--- nvim/lua/mnmx/plugins/conform.lua
+++ nvim/lua/mnmx/plugins/conform.lua
@@ -21,7 +21,22 @@ return {
       jsonnet = { "jsonnetfmt" },
       liquidsoap = { "liquidsoap-prettier" },
       hcl = { "terraform_fmt", "terragrunt_hclfmt" },
-      yaml = { "prettier" },
+
+      -- js
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+
+      -- web
+      css = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+      scss = { "prettierd", "prettier", stop_after_first = true },
+
+      -- markup
+      json = { "prettierd", "prettier", stop_after_first = true },
+      jsonc = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
     },
 
     format_on_save = function(bufnr)
